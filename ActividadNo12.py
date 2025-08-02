7531601f7647dee0535ca4b5582423967fa3909c
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-01 18:44:36 -0600

    modificacion

--- ActividadNo12.py
+++ ActividadNo12.py
@@ -1,28 +1,25 @@
-notas_1= []
+
 estudiantes=[]
-notas=0
 try:
     cantidad_estudiantes= int(input("Ingrese cantidad de estudiantes a ingresar:"))
 except ValueError:
     print("Error: Ingrese números validos...")
 else:
     for i in range(cantidad_estudiantes):
-        try:
-            nombre= input("ingrese nombre de estudiante:").lower()
-        except ValueError:
-            print("Error: deben ser valores validos.")
+        notas_1=[]
+        notas=0
+        nombre= input("ingrese nombre de estudiante:").lower()
 
         cantidad_notas= int(input("Ingrese cantidad de notas:"))
-    except ValueError:
         print("Error: Deben ser numeros validos.")
 
         for notas_estudiantes in range(cantidad_notas):
             print(f"Nota{notas_estudiantes +1}")
             nota= int(input(f"Ingrese nota {notas_estudiantes+1}"))
             notas_1.append(nota)
-            nota += notas
+            notas += nota
 
-        promedio = notas/len(notas)
+        promedio = notas/len(notas_1)
         estudiantes.append([nombre,promedio])
 
     for student in estudiantes:
